2a1557fd7785b843be52b0143ff8a4921740c39d
rmzvr <rmzvr@proton.me>
2026-02-03 20:35:41 +0100

    feat(cgi): add HTML form for sending JSON data and update test script for data handling

--- tests/test.js
+++ tests/test.js
@@ -2,7 +2,9 @@ function read()
 {
 	console.log("JS work!");
 	process.stdin.on("data", data => {
-		data = data.toString().toUpperCase()
+		data = data.toString();
+		// str = JSON.parse(data);
+		// console.log(str.name);
 		process.stdout.write(data + "\n")
 	})
 }
