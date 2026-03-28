29917e81b4cac05b168c79b59f309a4320a67fbe
rmzvr <rmzvr@proton.me>
2026-03-28 16:57:24 +0100

    test: update sleep.py script

--- data/cgi-bin/sleep.py
+++ data/cgi-bin/sleep.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
-import time
+import time, signal, os
+
+signal.signal(signal.SIGINT,  lambda *_: os._exit(0))
+signal.signal(signal.SIGTERM, lambda *_: os._exit(0))
 
 # Send valid CGI headers first (optional)
 print("Content-Type: text/plain\r\n\r")
